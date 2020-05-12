25892743421a5728f97e9b2e1462820f95e10b77
AKD1997 <63471513+AKD1997@users.noreply.github.com>
2020-05-12 15:38:31 -0700

    Changes in Internationalization message
    @Bean
    	public LocaleResolver localeResolvers() {
    		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
    		localeResolver.setDefaultLocale(Locale.US);
    		return localeResolver;
    	}
    Instead of "SessionLocaleResolver" what we can use is AcceptHeaderLocaleResolver  because we are sending the accept language in postman header.
    
    @Bean(name = "messageSource")
    	public ResourceBundleMessageSource bundleMessageSource() {
    		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
    		messageSource.setBasename("messages");
    		return messageSource;
    Here witout using this we can use we can use
    
    spring.messages.basename=messages
    in application.properties
    
    @RequestHeader(name = "Accept-Language", required = false) Locale locale
    Without using RequestHeader we can use LocaleContextHolder.getLocale()
    in get message
    		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());

--- restful-web-servicess/src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloContoler.java
+++ restful-web-servicess/src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloContoler.java
@@ -1,12 +1,10 @@
 package com.rest.webservices.restfulwebservices.helloworld;
 
-import java.util.Locale;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -32,8 +30,8 @@ public class HelloContoler {
 	}
 
 	@GetMapping(path = "/hello-world-internationalized")
-	public String helloWorldInternationalized(
-			@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
-		return messageSource.getMessage("good.morning.message", null, locale);
+	public String helloWorldInternationalized() {
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
+
 }
