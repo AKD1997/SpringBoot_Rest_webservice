53538c330d32b9f3197d52cf55b66ad3b9cccf46
AKD1997 <63471513+AKD1997@users.noreply.github.com>
2020-05-12 15:20:36 -0700

    Internationalization
    Different Country different laguage for that we are using Internationalization language

--- restful-web-servicess/src/main/java/com/rest/webservice/restfulwebservicess/RestfulWebServicessApplication.java
+++ restful-web-servicess/src/main/java/com/rest/webservice/restfulwebservicess/RestfulWebServicessApplication.java
@@ -1,13 +1,34 @@
 package com.rest.webservice.restfulwebservicess;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.rest.webservices.restfulwebservices.helloworld")
 public class RestfulWebServicessApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicessApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localeResolvers() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean(name = "messageSource")
+	public ResourceBundleMessageSource bundleMessageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
 }
