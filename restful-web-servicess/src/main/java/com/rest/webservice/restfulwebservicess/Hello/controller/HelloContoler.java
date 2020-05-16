09a1dbc5f10049ac7d5ba8d0a83a38e0d95b1e6c
AKD1997 <63471513+AKD1997@users.noreply.github.com>
2020-05-16 15:02:12 -0700

    Implementing_basic_Authentication
    Here We add spring-boot-starter-security dependency
                 <dependency>
    			<groupId>org.springframework.boot</groupId>
    			<artifactId>spring-boot-starter-security</artifactId>
    		</dependency>
    
    and also i enable sesurity at application.properties
    
    spring.security.user.name=Amit
    spring.security.user.password=kumar

--- restful-web-servicess/src/main/java/com/rest/webservice/restfulwebservicess/Hello/controller/HelloContoler.java
+++ restful-web-servicess/src/main/java/com/rest/webservice/restfulwebservicess/Hello/controller/HelloContoler.java
@@ -15,11 +15,6 @@ public class HelloContoler {
 	@Autowired
 	private MessageSource messageSource;
 
-	@GetMapping("/hello-world")
-	public String helloWorld() {
-		return "Hello World";
-	}
-
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
